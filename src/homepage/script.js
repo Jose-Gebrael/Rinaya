3a57e91649ce9d05c733c4beb41b6fa0e9abdfc5
Jose-Gebrael <jose.gebrayel@isae.edu.lb>
2025-06-20 20:14:13 +0300

    Homepage done.

--- src/homepage/script.js
+++ src/homepage/script.js
@@ -28,3 +28,16 @@ document.querySelectorAll(".faqToggle").forEach((toggle) => {
     }
   });
 });
+
+// Control Burger Menu
+const burger = document.getElementById("burger");
+const menu = document.getElementById("burgerMenu");
+const close = document.getElementById("closeBurger");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
+
+close.addEventListener("click", () => {
+  menu.classList.remove("show");
+});
